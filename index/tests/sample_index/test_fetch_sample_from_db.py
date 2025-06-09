d7ba3d56efd01da5e6a9ae518ad2bb34cbbdad28
Ola Austine <olaaustine@ebi.ac.uk>
2025-06-09 15:32:59 +0100

    Adding the test for population and sample

---
+++ index/tests/sample_index/test_fetch_sample_from_db.py
@@ -0,0 +1,56 @@
+import pytest 
+from unittest.mock import MagicMock, patch 
+from index.sample_index.fetch_samples_from_db import SampleDetailsFetcher
+
+@pytest.fixture
+def db_config():
+    return {
+        "host": "localhost",
+        "port": 3306,
+        "user": "user",
+        "password": "pass",
+        "database": "test_db",
+    }
+# @pytest.fixture
+# def mock_dict_structure():
+#     return {
+#         "biosampleId": None,
+#         "sex": None,
+#         "name": None,
+#         "source": [],
+#         "populations": [],
+#         "dataCollections": []
+#     }
+
+@pytest.fixture
+def fetcher(db_config):
+    return SampleDetailsFetcher(db_config)
+
+def test_fetch_samples(mocker, fetcher):
+    mock_cursor = MagicMock()
+    mock_cursor.fetchall.return_value = [
+        (1, "Sample1", "TEST123", "M")
+    ]
+    mock_db = MagicMock()
+    mock_db.cursor.return_value = mock_cursor
+    mocker.patch("index.sample_index.fetch_samples_from_db.connect", return_value=mock_db)
+
+    result = fetcher.fetch_samples()
+    assert len(result) == 1
+    assert result[0][2] == "TEST123"
+
+
+def test_fetch_source_samples(mocker, fetcher):
+    mock_cursor = MagicMock()
+    mock_cursor.fetchall.return_value = [
+        (1, "test_source", "test source description", 'http:/test_source_url' )
+    ]
+    mock_db = MagicMock()
+    mock_db.cursor.return_value = mock_cursor
+    mocker.patch("index.sample_index.fetch_samples_from_db.connect", return_value=mock_db)
+
+    result = fetcher.fetch_source_samples(1)
+    assert len(result) == 1
+    assert result[0][1] == "test_source"
+    assert result[0][2] == "test source description"
+
