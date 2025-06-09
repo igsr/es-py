62f4d2f91095f5671b4a59312acdb760f23c5e76
Ola Austine <olaaustine@ebi.ac.uk>
2025-06-09 15:44:49 +0100

    Changes for the test samples

--- index/tests/sample_index/test_fetch_sample_from_db.py
+++ index/tests/sample_index/test_fetch_sample_from_db.py
@@ -1,6 +1,7 @@
-import pytest 
-from unittest.mock import MagicMock, patch 
+import pytest
+from unittest.mock import MagicMock, patch
 from index.sample_index.fetch_samples_from_db import SampleDetailsFetcher
+
 
 @pytest.fixture
 def db_config():
@@ -11,6 +12,8 @@ def db_config():
         "password": "pass",
         "database": "test_db",
     }
+
+
 # @pytest.fixture
 # def mock_dict_structure():
 #     return {
@@ -22,18 +25,20 @@ def db_config():
 #         "dataCollections": []
 #     }
 
+
 @pytest.fixture
 def fetcher(db_config):
     return SampleDetailsFetcher(db_config)
 
+
 def test_fetch_samples(mocker, fetcher):
     mock_cursor = MagicMock()
-    mock_cursor.fetchall.return_value = [
-        (1, "Sample1", "TEST123", "M")
-    ]
+    mock_cursor.fetchall.return_value = [(1, "Sample1", "TEST123", "M")]
     mock_db = MagicMock()
     mock_db.cursor.return_value = mock_cursor
-    mocker.patch("index.sample_index.fetch_samples_from_db.connect", return_value=mock_db)
+    mocker.patch(
+        "index.sample_index.fetch_samples_from_db.connect", return_value=mock_db
+    )
 
     result = fetcher.fetch_samples()
     assert len(result) == 1
@@ -43,14 +48,15 @@ def test_fetch_samples(mocker, fetcher):
 def test_fetch_source_samples(mocker, fetcher):
     mock_cursor = MagicMock()
     mock_cursor.fetchall.return_value = [
-        (1, "test_source", "test source description", 'http:/test_source_url' )
+        (1, "test_source", "test source description", "http:/test_source_url")
     ]
     mock_db = MagicMock()
     mock_db.cursor.return_value = mock_cursor
-    mocker.patch("index.sample_index.fetch_samples_from_db.connect", return_value=mock_db)
+    mocker.patch(
+        "index.sample_index.fetch_samples_from_db.connect", return_value=mock_db
+    )
 
     result = fetcher.fetch_source_samples(1)
     assert len(result) == 1
     assert result[0][1] == "test_source"
     assert result[0][2] == "test source description"
-
